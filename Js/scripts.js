15f1f1c14ebb47560e30a6f5b8ae96f4b72fb6be
julieprograms <junge.julie@web.de>
2021-11-09 17:14:26 +0100

    added array pokemonList with 3 entries

---
+++ Js/scripts.js
@@ -0,0 +1,17 @@
+let pokemonList = [
+  {number: 1,
+    name: 'Bulbasaur',
+    type: ['grass', 'poison'],
+    height: 0.7
+  },
+  {number: 4,
+    name: 'Charmander',
+    type: 'fire',
+    height: 0.6
+  },
+  {number: 7,
+    name: 'Squirtle',
+    type: 'water',
+    height: 0.5
+  }
+];
