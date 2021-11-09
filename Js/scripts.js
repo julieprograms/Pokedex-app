fda006bfecef51ad2f43a7993ddf79c98defefc1
julieprograms <90261981+julieprograms@users.noreply.github.com>
2021-11-09 01:25:58 +0100

    Add files via upload

---
+++ Js/scripts.js
@@ -0,0 +1,24 @@
+
+//this is for testing only:
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+/* this is a longer comment
+with line breaks so
+you don't forget to comment the
+heck out of this! */
+
+//test some math stuff here
+/* let simpleAddition = 324 + 23455;
+document.write(simpleAddition);
+
+let size = 3205;
+let doubleSize = size * 53;
+document.write(doubleSize); */
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
