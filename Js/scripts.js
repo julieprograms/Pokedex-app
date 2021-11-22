b2877773217cfcca5374c18ab6c707ac922ce6b9
julieprograms <junge.julie@web.de>
2021-11-22 23:47:54 +0100

    added modal

--- Js/scripts.js
+++ Js/scripts.js
@@ -70,6 +70,63 @@ let pokemonRepository = (function () {
     });
   }
 
+//add modal, start:
+let modalContainer = document.querySelector('#modal-container');
+let modalField = document.querySelector('#modal-container.is-visible');
+
+function showModal(title, text) {
+
+  //clear modal of text
+  modalContainer.innerHTML ='';
+
+  let modal = document.createElement('div');
+  modal.classList.add('modal');
+
+  //add new modal content
+  let closeButtonElement = document.createElement('button');
+  closeButtonElement.classList.add('modal-close');
+  closeButtonElement.innerText = 'Close';
+  closeButtonElement.addEventListener('click', hideModal);
+
+  let titleElement = document.createElement('h1');
+  titleElement.innerText = title;
+
+  let contentElement = document.createElement('p');
+  contentElement.innerText = text;
+
+  modal.appendChild(closeButtonElement);
+  modal.appendChild(titleElement);
+  modal.appendChild(contentElement);
+  modalContainer.appendChild(modal);
+
+  modalContainer.classList.add('is-visible');
+}
+
+function hideModal() {
+  modalContainer.classList.remove('is-visible');
+}
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+modalContainer.addEventListener('click', (e) => {
+    //only close when user clicks directly on overlay not inside modal
+    let target = e.target;
+    if (target === modalContainer){
+      hideModal();
+    }
+  });
+
+document.querySelector('#show-modal').addEventListener('click', () => {
+  showModal('Modal title', 'This is the modal content!');
+});
+// end modal
+
+
+
 //access outside the IIFE
   return {
     add: add,
